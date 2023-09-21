f848c3f5bd70595fb42dd350e46690b7c349d2ae
Felipe Xavier <felipe.cvxavier@gmail.com>
2023-09-21 09:04:19 -0300

    up

--- src/Desafio2_velocidadeDowload.java
+++ src/Desafio2_velocidadeDowload.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Desafio2 {
+public class Desafio2_velocidadeDowload {
 
 	public static void main(String[] args) {
 		 Scanner scanner = new Scanner(System.in);
